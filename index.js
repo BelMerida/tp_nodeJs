e2f1ef9efa3660f36bee777845e21cd4b590656f
Belen <merida94.bm@gmail.com>
2021-01-14 14:06:54 -0300

    Agregado del Ejercicio 3 y Ejercicio 7

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const validacion = require('./validacion');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static(__dirname + '/personas'));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.json());
+
+app.get('/test', (req, res) =>{
+    res.json({
+        status: 'GET realizado',
+    });
+});
+
+app.post('/personas', (req, res) => {
+    validacion.validarDatos(req.body);
+    
+    const{nombre, apellido, dni} = req.body;
+
+    var rp = require('request-promise');
+
+    var options ={
+        method: 'POST',
+        uri: 'https://reclutamiento-14cf.firebaseio.com/personas.json',
+        body: {
+            nombre: nombre,
+            apellido: apellido,
+            dni: dni
+        },
+        json: true
+    };
+
+    rp(options)
+        .then(function (parseBody) {
+            console.log("Error al crear.")
+        })
+        .catch(function (err) {
+            console.log("Error al crear.")
+        });
+
+        res.status(201).json({
+            status: 'POST correcto.'
+        });
+});
+
+app.use((error, req, res, next) =>{
+    res.status(400).json({
+        status: 'error',
+        message: error.message,
+    });
+});
+
+app.listen(8000, () => console.log('Servidor inicializado en el puerto: 8000.'));
---
+++ validacion.js
@@ -0,0 +1,28 @@
+function validarDatos(data){
+    let { nombre, apellido, dni} = data;
+    dni = parseInt(dni);
+
+    if( apellido === NULL || apellido == ""){
+        throw new Error('El campo Apellido es obligatorio')
+    }
+    if( dni === NULL ){
+        throw new Error('El campo DNI es obligatorio')
+    }
+    if( typeof nombre !== "string"){
+        throw new Error('El nombre debe ser un String')
+    }
+    if( typeof apellido !== "string"){
+        throw new Error('El Apellido debe ser un String')
+    }
+    if( dni.toString().length > 10){
+        throw new Error('El DNI solo 10 caracteres como maximo')
+    }
+
+    if(Object.keys(data).length > 3){
+        throw new Error('La cantidad maxima de atributos son 3')
+    }
+}
+
+module.exports = {
+    validarDatos,
+};
